542f4be09cbea010f332aa551a98cd2835828f40
Chuniversity <contact@chunis.org>
2021-03-31 19:58:52 -0400

    Feature: Home Feed

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ $(document).ready(function () {
   var $app = $('#app');
   $app.html('');
   var $title = $('<h1>Twiddler</h1>');
+  var $updateBtn = $('<button id="update-feed">Update Feed</button>');
+  var $feed = $('<div id="#feed"></div>');
+
+
+
   $title.appendTo($app);
+  $updateBtn.appendTo($app);
+  $feed.appendTo($app);
+
 
   $title.on("click", function (event) {
     console.log(event);
@@ -10,15 +18,23 @@ $(document).ready(function () {
   });
 
 
-  var index = streams.home.length - 1;
-  while (index >= 0) {
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweet"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($app);
-    index -= 1;
-  }
+  $updateBtn.on("click", function (event) {
+    $feed.html('');
+    renderFeed()
+  })
 
 
+  var renderFeed = function () {
+    var index = streams.home.length - 1;
+    while (index >= 0) {
+      var tweet = streams.home[index];
+      var $tweet = $('<div class="tweet"></div>');
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      $tweet.appendTo($feed);
+      index -= 1;
+    }
+  };
+
+  renderFeed()
 
 });
