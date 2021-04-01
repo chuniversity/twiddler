408adff74d4811fecd16e3ff6de358974574719b
Chuniversity <contact@chunis.org>
2021-03-31 20:19:56 -0400

    quality and readability

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 $(document).ready(function () {
   var $app = $('#app');
   $app.html('');
+
+  // Create new HTML elements
+
   var $title = $('<h1>Twiddler</h1>');
+  var $subtitle = $('<h2>Where Twiddlers be Twiddlin\'</h2>');
   var $updateBtn = $('<button id="update-feed">Update Feed</button>');
   var $feed = $('<div id="#feed"></div>');
 
+  // Create event handler functions
 
+  var handleBtnClick = function (event) {
+    $feed.html('');
+    renderFeed()
+  }
+
+  var handleTitleClick = function (event) {
+    var titleType = event.target.localName === 'h1' ? 'title' : 'subtitle';
+    alert('The ' + titleType + ' of this page is: ' + event.target.innerText);
+  }
+
+  // Set event listeners
+
+  $title.on('click', handleTitleClick);
+  $subtitle.on('click', handleTitleClick);
+  $updateBtn.on("click", handleBtnClick);
+
+  // Append new HTML elements to the DOM
 
   $title.appendTo($app);
+  $subtitle.appendTo($app);
   $updateBtn.appendTo($app);
   $feed.appendTo($app);
 
 
-  $title.on("click", function (event) {
-    console.log(event);
-    alert('The title of this page is: ' + event.target.innerText);
-  });
-
-
-  $updateBtn.on("click", function (event) {
-    $feed.html('');
-    renderFeed()
-  })
-
+  // renderFeed
 
   var renderFeed = function () {
     var index = streams.home.length - 1;
